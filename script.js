38988298801608de281b5cdbeb519eae846a2cfd
Mariangvds <mariangvds@gmail.com>
2024-11-05 00:30:50 +0100

    Assignment 2

--- script.js
+++ script.js
@@ -9,19 +9,51 @@ decreaseBtn.onclick = function() {
     count--;
     counterValue.textContent = count;
     }
+
+    if (count >= 10 && count < 20) {
+        document.getElementById("freeShipping").style.display = "inline";
+      } else {
+        document.getElementById("freeShipping").style.display = "none";
+      }
+    
+    if (count < 20) {
+        document.getElementById("increaseBtn").style.backgroundColor = "#fcefe1";
+      }  
+
+    if (count >= 20) {
+        document.getElementById("outOfStock").style.display = "inline";
+      } else {
+        document.getElementById("outOfStock").style.display = "none";
+      }  
 }
 
 increaseBtn.onclick = function(){
-    if (count < 10) {
-        count++;
-        counterValue.textContent = count;
-    }
-   else {
-    alert("You have reached the limit.");
+    count++;
+    counterValue.textContent = count;
+ // alert("You have reached the limit.");
+
+    if (count >= 10 && count < 20) {
+        document.getElementById("freeShipping").style.display = "inline";
+      } else {
+        document.getElementById("freeShipping").style.display = "none";
+      }
+
+    if (count >= 20) {
+        document.getElementById("increaseBtn").style.backgroundColor = "#b73d0c";
+      }
+
+    if (count >= 20) {
+        document.getElementById("outOfStock").style.display = "inline";
+      } else {
+        document.getElementById("outOfStock").style.display = "none";
+      }  
+
    }
-}
 
 resetBtn.onclick = function(){
     count = 0;
     counterValue.textContent = count;
+    document.getElementById("increaseBtn").style.backgroundColor = "#fcefe1";
+    document.getElementById("freeShipping").style.display = "none";
+    document.getElementById("outOfStock").style.display = "none";
 }
